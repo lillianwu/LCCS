2032040a8075304d86f3a977e623b46f26845406
CHOON <choon_94@Hotmail.com>
2015-06-11 19:00:57 +0800

    add member session details

--- check_login.php
+++ check_login.php
@@ -9,13 +9,15 @@
 		$sId = clean($_POST["sId"]);
 		$password = clean($_POST["password"]);
 
-		$sql = "select mId,sId,name,permission from member where sId='$sId' and password='$password'";
+		$sql = "select * from member where sId='$sId' and password='$password'";
 		if($query = mysqli_query($conn, $sql)){
 			if(mysqli_num_rows($query) == 1){
 				$data = mysqli_fetch_array($query);
 				$_SESSION["mId"] = $data["mId"];
 				$_SESSION["sId"] = $data["sId"];
 				$_SESSION["name"] = $data["name"];
+				$_SESSION["nickName"] = $data["nickName"];
+				$_SESSION["email"] = $data["email"];
 				$_SESSION["permission"] = $data["permission"];
 				header("location: ./index.php");
 			}
